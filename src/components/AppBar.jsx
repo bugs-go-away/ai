4257d62ae032325483d65bb0be5be5b8f4ab3cc2
Garrett <garrettlchow@gmail.com>
2024-11-21 16:05:05 -0800

    added random button

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -15,11 +15,10 @@ import ChatSidebarContainer from './ChatSidebarContainer';
 const settings = ['Profile', 'Score', 'Logout'];
 
 function ResponsiveAppBar({ selectedPerson }) {
-
   const [anchorElUser, setAnchorElUser] = useState(null);
-	const [openSidebar, setOpenSidebar] = useState(false);
+  const [openSidebar, setOpenSidebar] = useState(false);
 
-	const toggleSidebar = (newOpen) => () => {
+  const toggleSidebar = (newOpen) => () => {
     setOpenSidebar(newOpen);
   };
 
@@ -32,75 +31,81 @@ function ResponsiveAppBar({ selectedPerson }) {
   };
 
   return (
-		<>
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-				<IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
+    <>
+      <AppBar position='static'>
+        <Container maxWidth='xl'>
+          <Toolbar disableGutters>
+            <IconButton
+              size='large'
+              edge='start'
+              color='inherit'
+              aria-label='open drawer'
               onClick={toggleSidebar(true)}
               sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-							flexGrow: 1,
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Social Skill Builder
-          </Typography>
-
-          <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/pro1.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              <MenuIcon />
+            </IconButton>
 
-		<ChatSidebarContainer open={openSidebar} toggleSidebar={toggleSidebar} person={selectedPerson}/>
-		</>
+            <Typography
+              variant='h6'
+              noWrap
+              component='a'
+              href='#app-bar-with-responsive-menu'
+              sx={{
+                flexGrow: 1,
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              Yap
+            </Typography>
+
+            <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
+              <Tooltip title='Open settings'>
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt='Remy Sharp' src='/pro1.jpg' />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id='menu-appbar'
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography sx={{ textAlign: 'center' }}>
+                      {setting}
+                    </Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <ChatSidebarContainer
+        open={openSidebar}
+        toggleSidebar={toggleSidebar}
+        person={selectedPerson}
+      />
+    </>
   );
 }
 export default ResponsiveAppBar;
