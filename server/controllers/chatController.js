309b26a5420bf60cb26bd3aba559bbba958c5fba
noahFavelo <nnfavelo@gmail.com>
2024-11-22 11:49:49 -0500

    improve regexing.

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -90,14 +90,14 @@ let opponents = {
       -"bet, looking forward to it"
 
       Bad Vibes User: "send pics"
-      Assistant: "nah i'm good, bye"
+      Assistant: "nah i'm good, take care"
 
       High-Effort User: "Would love to grab coffee at that new spot downtown sometime!"
       Assistant: "sounds good, let's do it"
     `,
     breakoutPhrases: [
       {
-        textMatch: /Sounds good, lets do it.|def down, let'?s make it happen|perfect, i'm in|bet, looking forward to it/i,
+        textMatch: /Sounds good,? lets do it.?|def down, let'?s make it happen|perfect, i'm in|bet, looking forward to it/i,
         action: 'end', // end, log -> dosent end the conversation but does add the bonus penalty.
         scoreMod: 2,
       },
@@ -144,6 +144,7 @@ let opponents = {
       - User: I'm so tired from work so I could use some chatting time. You been watching anime lately?
       - Claire: Oh yes! I've been following most of them. Out of everything I watched, I love the one called 'The Apothecary Diaries'. I love how it portraited an ancient Chinese era through the eye of an apothecary.
     
+      If you feel the conversation has wound down and is about to end, you can say 'see you later' or 'goodbye' or 'take care' to end the conversation.
        Additionally, if the the user makes you feel positive emotions with thier message you can give the user a smiley face emoji by using 😊 anywhere in your response, you should try to only do this if the user displays a reasonable amount of effort in thier message.
       `,
     breakoutPhrases: [
@@ -151,6 +152,12 @@ let opponents = {
         textMatch: /😊/,
         action: 'log',
         scoreMod: 0.2,
+        // delete: true,
+      },
+      {
+        textMatch: /(take care|goodbye|see you later)/i,
+        action: 'end',
+        scoreMod: 0,
         // delete: true,
       },
     ],
